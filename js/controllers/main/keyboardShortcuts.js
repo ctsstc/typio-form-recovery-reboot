bb9cdebad543215dfaa8b89af64d7d5347b43b57
Nicklas Sandell <nike.sandell@gmail.com>
2017-11-19 20:01:38 +0100

    Keyboard shorcut work again

--- js/controllers/main/keyboardShortcuts.js
+++ js/controllers/main/keyboardShortcuts.js
@@ -1,40 +1,41 @@
-/*'use strict';
-
 window.terafm = window.terafm || {};
-terafm.keyboardShortcuts = (function() {
+terafm.keyboardShortcuts = {};
 
-	this.setupKeyboardShortcuts = function() {
+(function(keyboardShortcuts, db, editableManager) {
+	'use strict';
 
-		Mousetrap.bindGlobal('ctrl+del', function(e) {
+	console.log('keyo');
 
-			var fields = terafm.db.getLatestSession(),
-				totalCount = Object.keys(fields).length,
-				fails = 0;
+	Mousetrap.bindGlobal('ctrl+del', function(e) {
+		console.log('restoring');
 
-			if(totalCount < 1) {
-				//terafm.toast.create('Nothing to restore.')
-				return false;
-			}
+		var fields = db.getLatestSession(),
+			totalCount = Object.keys(fields).length,
+			fails = 0;
 
-			for(var fieldId in fields) {
-				var editable = fields[fieldId];
-				var target = terafm.editableManager.getEditableByPath(editable.path, editable.frame);
+		if(totalCount < 1) {
+			//terafm.toast.create('Nothing to restore.')
+			return false;
+		}
 
-				if(target) {
-					terafm.editableManager.setEditableValue(target, editable.value);
-					terafm.editableManager.flashEditable(target);
-				} else {
-					fails++;
-				}
-			}
+		for(var fieldId in fields) {
+			var editable = fields[fieldId];
+			var target = editableManager.getEditableByPath(editable.path, editable.frame);
 
-			if(fails === 0) {
-				//terafm.toast.create('Recovered previous session.');
+			if(target) {
+				editableManager.setEditableValue(target, editable.value);
+				editableManager.flashEditable(target);
 			} else {
-				//terafm.toast.create(fails + ' previous entries could not be restored automatically. Open form recovery to restore fields manually.', 10*1000);
+				fails++;
 			}
+		}
 
-		});
-	}
+		if(fails === 0) {
+			//terafm.toast.create('Recovered previous session.');
+		} else {
+			//terafm.toast.create(fails + ' previous entries could not be restored automatically. Open form recovery to restore fields manually.', 10*1000);
+		}
 
-})();*/
+	});
+
+})(terafm.keyboardShortcuts, terafm.db, terafm.editableManager);
