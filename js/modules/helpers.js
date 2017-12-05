b806eeacb8f4ca105fdf17ad43eba10d54f829c9
Nicklas Sandell <nike.sandell@gmail.com>
2017-12-05 15:09:16 +0100

    Removed old helper code

--- js/modules/helpers.js
+++ js/modules/helpers.js
@@ -29,7 +29,6 @@ terafm.help = (function() {
 	}
 
 	exp.cloneObject = function(orgObj) {
-		// return JSON.parse(JSON.stringify(orgObj));
 		return Object.assign({}, orgObj);
 	}
 
@@ -92,40 +91,6 @@ terafm.help = (function() {
 		var parent = elem.parentElement;
 		if (!parent) return undefined;
 		return fn(parent) ? parent : parentElem(parent, cb);
-	}
-
-	// Deprecated
-/*	exp.deepQuerySelectorAll = function(selector) {
-		var frames = window.top.document.querySelectorAll('iframe'),
-			matches = [];
-
-		frames.forEach(function(frame) {
-			try{
-				var fres = frame.contentDocument.querySelectorAll(selector);
-				Array.prototype.push.apply(matches, fres);
-			} catch(e) {};
-		});
-
-		var topres = window.top.document.querySelectorAll(selector);
-		Array.prototype.push.apply(matches, topres);
-
-		return matches.length ? matches : false;
-	}
-*/
-	// https://stackoverflow.com/questions/442404/retrieve-the-position-x-y-of-an-html-element
-	exp.getElemOffset = function( el ) {
-		var _x = 0;
-		var _y = 0;
-		while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
-			_x += el.offsetLeft - el.scrollLeft;
-			_y += el.offsetTop - el.scrollTop;
-			el = el.offsetParent;
-		}
-
-		// Added
-		_y += window.scrollY;
-		_x += window.scrollX;
-		return { top: _y, left: _x };
 	}
 
 	return exp;
