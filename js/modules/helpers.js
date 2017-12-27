683d7f448dab22370481a85fdd65f8e56489b0f7
Nicklas Sandell <nike.sandell@gmail.com>
2017-12-27 16:14:21 +0100

    Capitalize "just now" in pretty date

--- js/modules/helpers.js
+++ js/modules/helpers.js
@@ -61,7 +61,7 @@ terafm.help = (function() {
 			return 'Yesterday at ' + date.getHours() + ':' + date.getMinutes();
 		}
 
-		return day_diff == 0 && (diff < 60 && "just now" || diff < 120 && "1 minute ago" || diff < 3600 && Math.floor(diff / 60) + " minutes ago" || diff < 7200 && "1 hour ago" || diff < 86400 && Math.floor(diff / 3600) + " hours ago");
+		return day_diff == 0 && (diff < 60 && "Just now" || diff < 120 && "1 minute ago" || diff < 3600 && Math.floor(diff / 60) + " minutes ago" || diff < 7200 && "1 hour ago" || diff < 86400 && Math.floor(diff / 3600) + " hours ago");
 	}
 
 
