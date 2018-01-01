d09f146dea3db474120c8bee6ab0672467254c5f
Nicklas Sandell <nike.sandell@gmail.com>
2018-01-01 19:38:44 +0100

    Deleted old duplicate of blacklist file and added alert for clearData action

--- js/controllers/blacklisted.js
+++ js/controllers/blacklisted.js
@@ -3,7 +3,7 @@
 	
 	// Used to check if script is already injected. Message is sent from background.js
 	chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-
+		
 		// Used to check if content scripts are already injected
 		if(request.action === 'ping') {
 			sendResponse(true);
@@ -14,10 +14,12 @@
 		} else if(request.action === 'openRecoveryDialog') {
 			showError();
 
+		} else if(request.action === 'clearData') {
+			alert('Your data cannot be deleted because Typio is disabled on this domain. If Typio is enabled, try refreshing the page before deleting the data')
 		}
 	});
 
 	function showError() {
-		alert('You have disabled Typio on this website. Please open the settings and remove ' + window.location.hostname + ' from the blacklist.');
+		alert('You have disabled Typio on this domain. Please open the settings and remove ' + window.location.hostname + ' from the blacklist.');
 	}
 })();
