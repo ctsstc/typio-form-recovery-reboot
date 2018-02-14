c20346ddfd4c2d765847206317c7e269ca23906d
Nicklas <nike.sandell@gmail.com>
2018-02-14 21:07:18 +0100

    Removed console logs in toast module

--- js/modules/toast.js
+++ js/modules/toast.js
@@ -5,22 +5,17 @@ terafm.toast = {};
 
 	var rootNode;
 
-	console.log('toast init')
-
 	toast.create = function(text) {
 		build(function() {
 			rootNode.innerHTML = '<div class="toast">'+ text +'</div>';
-			console.log('toast create')
 		})
 	}
 
 	function build(callback) {
-		console.log('building')
 		ui.inject({
 			path: 'templates/toast.tpl',
 			returnNode: '#toast-container'
 		}, function(resnode) {
-			console.log('built ok')
 			rootNode = resnode;
 			callback();
 		})
