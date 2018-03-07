d11250640927065d8b21f3e88aaef80c75e059a3
Nicklas <nike.sandell@gmail.com>
2018-03-07 20:12:48 +0100

    Work on popuptrigger and key shortcuts. Super hacky still.

--- js/controllers/content/popupTrigger.js
+++ js/controllers/content/popupTrigger.js
@@ -2,8 +2,7 @@ window.terafm = window.terafm || {};
 
 (function(DOMEvents, initHandler, ui, help, editableManager) {
 
-	var node,
-		currEditable;
+	var node;
 
 	initHandler.onInit(function() {
 		addEventListeners();
@@ -13,15 +12,27 @@ window.terafm = window.terafm || {};
 	function addEventListeners() {
 		makeThing();
 
+		node.addEventListener('click', e => e.preventDefault() )
+		node.addEventListener('mousedown', function(e) {
+			e.preventDefault();
+			e.stopPropagation();
+
+			terafm.contextMenuController.setContext(terafm.focusedEditable, {x: parseFloat(node.style.left)+15, y: parseFloat(node.style.top)+15})
+			terafm.contextMenuController.open();
+		})
+
+		// On editable focus
 		DOMEvents.registerHandler('focus', function(e) {
 			var editable = e.path[0],
 				rect = editable.getBoundingClientRect();
 
 			if(editableManager.isEditableText(editable)) {
-				currEditable = editable;
+				terafm.focusedEditable = editable
 
-				node.style.top = rect.top + window.scrollY + 3 + 'px';
-				node.style.left = rect.left + rect.width + window.scrollX - 24 + 'px';
+				var rect = terafm.editableManager.getRect(editable)
+
+				node.style.top = rect.y + 4 + 'px';
+				node.style.left = rect.x + rect.width - 20 + 'px';
 				
 				node.style.display = 'block';
 			}
@@ -31,30 +42,17 @@ window.terafm = window.terafm || {};
 			node.style.display = 'none'
 		});
 
-
-		node.addEventListener('mousedown', function(e) {
-			setTimeout(function() {
-				terafm.contextMenuController.setContext(currEditable, {x: e.pageX, y: e.pageY})
-				terafm.contextMenuController.open();
-			}, 10)
-		})
-
 	}
 
 	function makeThing() {
 		if(!node) {
 			ui.inject({
-				html: `<a id="popupTrigger" href="#" style="background: yellow; border-radius: 2px; opacity: .5; width: 16px; height: 16px; position: absolute; top: 0; left: 0; z-index: 999999999;"></a>`,
+				html: `<a id="popupTrigger" href="#" style="background: yellow; border-radius: 2px; opacity: .5; width: 16px; height: 16px; display: none;  position: absolute; top: 0; left: 0; z-index: 999999999;"></a>`,
 				returnNode: '#popupTrigger'
 			}, function(res) {
 				node = res;
 			})
 		}
-	}
-
-	function getEditablePos(el) {
-		var pos = el.getBoundingClientRect()
-		return pos
 	}
 
 })(terafm.DOMEvents, terafm.initHandler, terafm.ui, terafm.helpers, terafm.editableManager);
