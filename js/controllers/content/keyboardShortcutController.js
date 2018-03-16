c4dae5c6c54705804793cf1835cf31ebfecf60d9
Nicklas <nike.sandell@gmail.com>
2018-03-16 16:53:33 +0100

    Added keyboard shortcut popup

--- js/controllers/content/keyboardShortcutController.js
+++ js/controllers/content/keyboardShortcutController.js
@@ -1,11 +1,22 @@
-(function(db, editableManager, initHandler, options, keyboardShortcuts, toast) {
+window.terafm = window.terafm || {};
+
+terafm.keyboardShortcutController = {};
+
+(function(keyboardShortcutController, db, editableManager, initHandler, options, keyboardShortcuts, toast) {
 	'use strict';
 
+	var popupNode,
+		popupVisible = false;
+
+	keyboardShortcutController.showShortcutDialog = function() {
+		showPopup();
+	}
+
 	initHandler.onInit(function() {
+
 		if(options.get('keybindEnabled')) {
 
 			var keybindRestorePreviousSession = options.get('keybindRestorePreviousSession');
-
 			if(keybindRestorePreviousSession.length) keyboardShortcuts.on(keybindRestorePreviousSession, function() {
 				toast.create('Restoring previous session')
 				
@@ -29,8 +40,68 @@
 					}
 				}
 			});
-
 		}
 	})
 
-})(terafm.db, terafm.editableManager, terafm.initHandler, terafm.options, terafm.keyboardShortcuts, terafm.toast);
+	function showPopup() {
+		build(function() {
+			popupNode.classList.remove('hidden');
+			popupVisible = true;
+		})
+	}
+
+	function hidePopup() {
+		if(popupVisible) {
+			popupNode.classList.add('hidden')
+			popupVisible = false;
+		}
+	}
+
+	function prettyKeyCombo(keyarr) {
+		return '<span class="key">' + keyarr.join('</span> <span class="key">') + '</span>';
+	}
+
+	function setupEventListeners() {
+		console.log('ev list')
+		popupNode.addEventListener('click', function(e) {
+			var target = e.path[0];
+
+			// Close btn
+			if(target.dataset.action === 'close') {
+				hidePopup();
+
+			// Overlay
+			} else if(target.id === 'keyboardShortcutPopup') {
+				hidePopup();
+
+			// Opts link
+			} else if(target.dataset.action === 'open-options') {
+				chrome.runtime.sendMessage({action: 'openSettings'});
+			}
+		})
+
+		keyboardShortcuts.on(['Escape'], function() {
+			hidePopup();
+		})
+	}
+
+	function build(callback) {
+		if(!popupNode) {
+			terafm.ui.inject({
+				path: 'templates/keyboardShortcutPopup.tpl',
+				returnNode: '#keyboardShortcutPopup'
+			}, {
+				'{{keybindToggleRecDiag}}' : prettyKeyCombo(options.get('keybindToggleRecDiag')),
+				'{{keybindRestorePreviousSession}}' : prettyKeyCombo(options.get('keybindRestorePreviousSession')),
+				'{{keybindOpenQuickAccess}}' : prettyKeyCombo(options.get('keybindOpenQuickAccess'))
+			}, function(res) {
+				popupNode = res;
+				setupEventListeners();
+				callback()
+			})
+		} else {
+			callback()
+		}
+	}
+
+})(terafm.keyboardShortcutController, terafm.db, terafm.editableManager, terafm.initHandler, terafm.options, terafm.keyboardShortcuts, terafm.toast);
---
+++ templates/keyboardShortcutPopup.tpl
@@ -0,0 +1,46 @@
+<div id="keyboardShortcutPopup">
+	<div class="modal">
+
+		<div class="modal-header">
+			<p class="title">Typio Keyboard Shortcuts</p>
+			<button data-action="close" class="close">
+				<svg version="1.1" viewBox="0 0 24 24" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g id="info"/><g id="icons"><path d="M14.8,12l3.6-3.6c0.8-0.8,0.8-2,0-2.8c-0.8-0.8-2-0.8-2.8,0L12,9.2L8.4,5.6c-0.8-0.8-2-0.8-2.8,0   c-0.8,0.8-0.8,2,0,2.8L9.2,12l-3.6,3.6c-0.8,0.8-0.8,2,0,2.8C6,18.8,6.5,19,7,19s1-0.2,1.4-0.6l3.6-3.6l3.6,3.6   C16,18.8,16.5,19,17,19s1-0.2,1.4-0.6c0.8-0.8,0.8-2,0-2.8L14.8,12z" id="exit"/></g></svg>
+			</button>
+		</div>
+
+		<div class="modal-content">
+
+			<!-- <p class="group-title">General</p> -->
+			<div class="combo-group">
+				<div class="combo">
+					<p class="description">Open/Close recovery dialog</p>
+					<p class="keys">{{keybindToggleRecDiag}}</p>
+				</div>
+				<div class="combo">
+					<p class="description">Restore previous session</p>
+					<p class="keys">{{keybindRestorePreviousSession}}</p>
+				</div>
+			</div>
+
+			<!-- <p class="group-title">Quick Restore</p> -->
+			<div class="combo-group">
+				<div class="combo">
+					<p class="description">Open Quick Restore for focused field</p>
+					<p class="keys">{{keybindOpenQuickAccess}}</p>
+				</div>
+				<div class="combo">
+					<p class="description">Navigate items</p>
+					<p class="keys"><span class="key">▲</span> <span class="key">▼</span></p>
+				</div>
+				<div class="combo">
+					<p class="description">Select item</p>
+					<p class="keys"><span class="key">Space</span></p>
+				</div>
+			</div>
+
+
+			<div style="text-align: center;"><a data-action="open-options" href="#">Change keyboard combinations in options</a></div>
+
+		</div>
+	</div>
+</div>
