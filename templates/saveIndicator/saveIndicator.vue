661f54dba88b0f8bbf614a4310f0886ee202cbef
Nicklas <nike.sandell@gmail.com>
2018-05-07 17:24:52 +0200

    Converted saveindicator to vue

---
+++ templates/saveIndicator/saveIndicator.vue
@@ -0,0 +1,3 @@
+<div v-bind:class="[isVisible ? 'visible' : '', styleClass]" :style="'background-color: ' + hexColor" title="This is the save indicator for Typio Form Recovery. Disable or change indicator style in the settings." id="save-indicator">
+	<div class="animator"></div>
+</div>
