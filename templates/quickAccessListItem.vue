7d894b3a144f66ad82d50566f4f683b4a5742991
Nicklas <nike.sandell@gmail.com>
2018-05-12 20:38:37 +0200

    Further work on QA sub-menus

--- templates/quickAccessListItem.vue
+++ templates/quickAccessListItem.vue
@@ -1,8 +1,18 @@
-<li @click="restore()" @mousemove="preview();" @mouseleave="resetPreview()" v-bind:class="[selected ? 'selected' : '', 'selectable']">
-
-	<div class="fill">{{ entry.getPrintableValue({truncate: 50}) }}</div>
+<li @click="commit()" @mouseenter="select" @mouseleave="unselect" v-bind:class="[selected ? 'selected' : '', 'selectable']">
 	
-	<div v-if="isSess" class="flex-icon">
-		<span class="icon icon-arrow-forward">{{ entry.getSession().length }}</span>
+	<template v-if="itemType === 'entry'">
+		<div class="fill">{{ entry.getPrintableValue({truncate: 50}) }}</div>
+
+		<div v-if="isSub" class="flex-icon thin">
+			<span :class="[entry.hasEditable() ? 'status ok' : 'status bad']"></span>
+		</div>
+	</template>
+
+	<template v-if="itemType === 'link'">
+		<div class="fill">{{ itemText }}</div>
+	</template>
+	
+	<div v-if="hasSub" class="flex-icon thin">
+		<span class="icon icon-arrow-forward"></span>
 	</div>
 </li>
