f820fc4000f368349ce2ef8cdedf80ee7ff812ef
Nicklas <nike.sandell@gmail.com>
2018-11-03 12:16:14 +0100

    Work on quick access

--- templates/quickAccessListItem.vue
+++ templates/quickAccessListItem.vue
@@ -1,18 +1,24 @@
-<li v-if="itemType !== 'entry' || (itemType === 'entry' && entry.isTextType())" @click="commit()" @mouseenter="select" @mouseleave="unselect" v-bind:class="[selected ? 'selected' : '', 'selectable', itemSize && 'size-' + itemSize]">
-	
-	<template v-if="itemType === 'entry'">
-		<div class="fill">{{ entry.getPrintableValue({truncate: 50}) }}</div>
-	</template>
+<li v-if="itemType !== 'entry' || (itemType === 'entry' && entry.isTextType())">
 
-	<template v-if="itemType === 'link' && itemText">
-		<div class="fill">{{ itemText }}</div>
-	</template>
-	<template v-if="itemType === 'link' && itemImg">
-		<span v-bind:class="['icon', itemImg]"></span>
-	</template>
+	<div @click="commit()" @mouseenter="select" @mouseleave="unselect" v-bind:class="[selected ? 'selected' : '', 'selectable', itemSize && 'size-' + itemSize, 'fill']" :data-tooltip="itemTooltip">
+		
+		<template v-if="itemType === 'entry'">
+			<span v-if="!isSess" class="icon inner-fake-arrow icon-arrow-forward"><span data-tooltip="Restore this entry (this entry was typed in another field)"></span></span>
+			{{ entry.getPrintableValue({truncate: 80}) }}
+		</template>
 
-	<div v-if="isSess" class="flex-icon thin">
+		<template v-if="itemType === 'link' && itemText">
+			{{ itemText }}
+		</template>
+		<template v-if="itemType === 'link' && itemImg">
+			<span v-bind:class="['icon', itemImg]"></span>
+		</template>
+			
+	</div>
+
+	<div data-tooltip="Restore just this entry." v-if="isSess" @click="commit(true)" @mouseenter="singleSelect" @mouseleave="unselect" v-bind:class="[singleSelected ? 'selected' : '', 'selectable', itemSize && 'size-' + itemSize, 'flex-icon']">
 		{{ entry.session.length }}
 		<span class="icon icon-arrow-forward"></span>
 	</div>
+
 </li>
