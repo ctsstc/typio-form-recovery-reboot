c095bfc954febc9b015fbdc476c3d3dff04404e5
Nicklas <nike.sandell@gmail.com>
2018-11-04 15:17:18 +0100

    Begone gitignores!

--- templates/tmp/toast.js
+++
@@ -1,3 +0,0 @@
-function render() {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{class:[_vm.isVisible ? 'visible' : ''],attrs:{"id":"toast-container"}},[_c('p',{staticClass:"message"},[_vm._v(_vm._s(_vm.message))])])}
-var staticRenderFns = [];
-export {render, staticRenderFns}
