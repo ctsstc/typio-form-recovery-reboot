578662eb00f629a80e861c318d3a9356869fbc2a
Nicklas <nike.sandell@gmail.com>
2019-05-29 15:19:50 +0200

    Cleanup

---
+++ js/controllers/content/tmp/contentController.js
@@ -0,0 +1,42 @@
+var terafm = window.terafm;
+
+(function(db, options, recoveryDialog, initHandler, Events, editableManager) {
+	'use strict';
+
+
+
+	terafm.blacklist.isBlocked(window.location.href, function(blocked) {
+		if(blocked) {
+			terafm.isBlocked = true;
+		
+		} else {
+			// Load extension options into memory
+			options.loadFromChromeStorage(function() {
+				
+				// Initiate DB, populate in-memory storage
+				db.init(function() {
+
+					// Run init handlers
+					initHandler.executeInitHandlers();
+					console.log('Typio is ready!');
+				});
+			});
+		}
+	});
+
+
+	// Messages from background.js
+	chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+		if(request.action === 'clearData') {
+			if(terafm.isBlocked) return terafm.blockController.warn();
+			db.deleteAllDataForDomain();
+			terafm.toastController.create('Database cleared for ' + window.location.hostname);
+		}
+	});
+
+
+
+
+	
+
+})(terafm.db, terafm.options, terafm.recoveryDialog, terafm.initHandler, terafm.Events, terafm.editableManager);
