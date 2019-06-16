449701128b50939c4c68abd630532dfc6d3343dd
Nicklas Sandell <nicklas.sandell@seitwerk.de>
2019-06-16 09:41:40 +0200

    Modify build script to only copy used images to reduce package size

--- build/tasks/copy.js
+++ build/tasks/copy.js
@@ -3,7 +3,7 @@ const gulp = require('gulp');
 function copy_task(cb) {
     gulp.src('../fonts/**/*.*').pipe(gulp.dest('../publish/fonts'));
     gulp.src('../js/libs/**/*.*').pipe(gulp.dest('../publish/js'));
-    gulp.src('../img/**/*.png').pipe(gulp.dest('../publish/img')); // Todo: Clean up images folder
+    gulp.src('../img/used/**/*.png').pipe(gulp.dest('../publish/img')); // Todo: Clean up images folder
     gulp.src(['../manifest.json', '../license.txt']).pipe(gulp.dest('../publish'));
 
     cb();
