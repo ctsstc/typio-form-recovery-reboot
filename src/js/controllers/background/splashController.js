3ef259bd8d804ac4f1815a577b6c11dce581bc6a
Nick <you@example.com>
2020-08-31 17:50:10 +0200

    Add versioning to splash screen to avoid spam

--- src/js/controllers/background/splashController.js
+++ src/js/controllers/background/splashController.js
@@ -1,11 +1,19 @@
 (function() {
 
+	// Changing this number will make splash screen appear after update installation
+	const SPLASH_VERSION = 1;
+
 	chrome.runtime.onInstalled.addListener(function(details) {
-		if(false && details.reason === 'update') {
-			let url = chrome.extension.getURL('html/splash.html');
-			chrome.tabs.create({
-				url: url
-			})
+		if(details.reason === 'update') {
+			chrome.storage.sync.get('splashVersion', obj => {
+				if(!obj.hasOwnProperty('splashVersion') || obj.splashVersion !== SPLASH_VERSION) {
+					const url = chrome.extension.getURL('html/splash.html');
+					chrome.tabs.create({
+						url: url
+					})
+					chrome.storage.sync.set({ splashVersion: SPLASH_VERSION });
+				}
+			});
 		}
 	})
 	
