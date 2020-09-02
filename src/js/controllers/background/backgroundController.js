c29bb9492a1c8872ae0b40b86006f73f17490035
Nick <you@example.com>
2020-09-02 13:15:30 +0200

    Debug stuff

--- src/js/controllers/background/backgroundController.js
+++ src/js/controllers/background/backgroundController.js
@@ -7,3 +7,8 @@ import './splashController.js';
 
 // In case i need it for future stuff
 chrome.storage.sync.set({version: 2});
+// chrome.storage.local.clear();
+
+// chrome.tabs.create({
+//     url: 'chrome-extension://lpbjhmjpobgjcacflfibadcfimmbpmal/html/app.html#/database-manager',
+// })
