33bd2d448ad20538bafb6d5ae93cb33efb8ab2c8
Nick <you@example.com>
2020-09-15 12:54:18 +0200

    Fix frame injector issue

--- src/js/controllers/content.frameInjector/frameInjectorController.js
+++ src/js/controllers/content.frameInjector/frameInjectorController.js
@@ -1,13 +1,9 @@
 import Cache from '../../modules/Cache';
 import PathGenerator from '../../modules/PathGenerator';
 
-(() => {
-	// Run only in child frames
-	if(window === window.top) {
-		return;
-	}
-	
-	
+// Run only in child frames
+if(document !== window.top.document) {
+
 	function eventHandler(e) {
 		let msg = {
 			action: 'terafmEventCatcher',
@@ -33,16 +29,18 @@ import PathGenerator from '../../modules/PathGenerator';
 		window.top.postMessage(msg, '*');
 	}
 	
-	
-	document.addEventListener('input', eventHandler);
-	document.addEventListener('keyup', eventHandler, true);
-	document.addEventListener('contextmenu', eventHandler);
-	document.addEventListener('mousedown', eventHandler);
-	document.addEventListener('click', eventHandler);
-	document.addEventListener('dblclick', eventHandler);
-	document.addEventListener('focus', eventHandler, true);
-	document.addEventListener('blur', eventHandler, true);
-	document.addEventListener('change', eventHandler, true);
-	document.addEventListener('keydown', eventHandler, true);
-})();
-
+	// Wrapped in a timeout because some apps (like http://qiyukf.com/) does some wonky shit
+	// to the DOM after document load and the event listeners were not added.
+	setTimeout(() => {
+		document.addEventListener('input', eventHandler);
+		document.addEventListener('keyup', eventHandler, true);
+		document.addEventListener('contextmenu', eventHandler);
+		document.addEventListener('mousedown', eventHandler);
+		document.addEventListener('click', eventHandler);
+		document.addEventListener('dblclick', eventHandler);
+		document.addEventListener('focus', eventHandler, true);
+		document.addEventListener('blur', eventHandler, true);
+		document.addEventListener('change', eventHandler, true);
+		document.addEventListener('keydown', eventHandler, true);
+	})
+}
