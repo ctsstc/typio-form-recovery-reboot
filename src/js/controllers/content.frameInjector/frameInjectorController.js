fee15f70686bf8dff5744c768bdb7b4d2a6f82a4
Nick <you@example.com>
2020-09-17 20:37:08 +0200

    Frame injector event listener attachment fix

--- src/js/controllers/content.frameInjector/frameInjectorController.js
+++ src/js/controllers/content.frameInjector/frameInjectorController.js
@@ -4,6 +4,19 @@ import PathGenerator from '../../modules/PathGenerator';
 // Run only in child frames
 if(document !== window.top.document) {
 
+	const events = [
+		{ name: 'input' },
+		{ name: 'keyup', capture: true },
+		{ name: 'contextmenu' },
+		{ name: 'mousedown' },
+		{ name: 'click' },
+		{ name: 'dblclick' },
+		{ name: 'focus', capture: true },
+		{ name: 'blur', capture: true },
+		{ name: 'change', capture: true },
+		{ name: 'keydown', capture: true },
+	];
+
 	function eventHandler(e) {
 		let msg = {
 			action: 'terafmEventCatcher',
@@ -19,8 +32,6 @@ if(document !== window.top.document) {
 			msg.event.pageY = e.pageY;
 		} else if(e.type === 'keydown') {
 			msg.event.key = e.key;
-			// msg.event.ctrlKey = e.ctrlKey;
-			// msg.event.altKey = e.altKey;
 		} else if(e.type === 'keyup') {
 			msg.event.keyCode = e.keyCode;
 		}
@@ -28,19 +39,16 @@ if(document !== window.top.document) {
 		// console.log(msg);
 		window.top.postMessage(msg, '*');
 	}
+
+	function attachListeners() {
+		for(const event of events) {
+			document.addEventListener(event.name, eventHandler, event.capture);
+		}
+	}
+
+	attachListeners();
 	
 	// Wrapped in a timeout because some apps (like http://qiyukf.com/) does some wonky shit
-	// to the DOM after document load and the event listeners were not added.
-	setTimeout(() => {
-		document.addEventListener('input', eventHandler);
-		document.addEventListener('keyup', eventHandler, true);
-		document.addEventListener('contextmenu', eventHandler);
-		document.addEventListener('mousedown', eventHandler);
-		document.addEventListener('click', eventHandler);
-		document.addEventListener('dblclick', eventHandler);
-		document.addEventListener('focus', eventHandler, true);
-		document.addEventListener('blur', eventHandler, true);
-		document.addEventListener('change', eventHandler, true);
-		document.addEventListener('keydown', eventHandler, true);
-	})
+	// to the DOM after document load and the event listeners were not attached.
+	setTimeout(attachListeners, 20);
 }
