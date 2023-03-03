3def4eae72cb4563a8603a75f67ae46fa0ec85c0
Cody Swartz <ctsstc@gmail.com>
2023-03-02 21:32:24 -0800

    Update vue rendering to v3 for another one
    <!-- ps-id: c1109f5d-dca9-4c29-99f2-ebc16a36db05 -->

--- src/js/controllers/content/toastController.js
+++ src/js/controllers/content/toastController.js
@@ -1,37 +1,40 @@
-import ui from '../../modules/ui';
-import ToastComponent from '../../vue/backendApp/components/Toast.vue';
-import Vue, { createApp, h } from 'vue';
+import ui from "../../modules/ui";
+import ToastComponent from "../../vue/backendApp/components/Toast.vue";
+import Vue, { createApp, h } from "vue";
 let controller = {};
 
 let vue;
 
-controller.create = function(message) {
-	build(function() {
-		vue.showMessage(message);
-	});
-}
-
+controller.create = function (message) {
+  build(function () {
+    vue.showMessage(message);
+  });
+};
 
 function build(callback) {
-	if(vue) return callback && callback();
+  if (vue) return callback && callback();
 
-	ui.inject({
-		html: '<div id="tmp-toast-holder"></div>',
-		returnNode: '#tmp-toast-holder'
-	}, function(rootnode) {
-		makeVue(rootnode, () => {
-			if(callback) callback();
-		});
-	});
+  ui.inject(
+    {
+      html: '<div id="tmp-toast-holder"></div>',
+      returnNode: "#tmp-toast-holder",
+    },
+    function (rootnode) {
+      makeVue(rootnode, () => {
+        if (callback) callback();
+      });
+    }
+  );
 }
 
 function makeVue(rootnode, callback) {
+  vue = createApp({
+    render() {
+      return h(ToastComponent);
+    },
+  }).mount(rootnode);
 
-	vue = createApp({
-		render() { return h(ToastComponent)}
-	}).mount(rootnode)
-
-	if(callback) requestAnimationFrame(() => requestAnimationFrame(callback));
+  if (callback) requestAnimationFrame(() => requestAnimationFrame(callback));
 }
 
 export default controller;
