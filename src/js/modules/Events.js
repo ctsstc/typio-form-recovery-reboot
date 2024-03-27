86a1d73d9e648123c3dd8eac776571536db9a436
Cody Swartz <ctsstc@gmail.com>
2024-03-26 18:20:13 -0700

    Run formatter on Events.js

--- src/js/modules/Events.js
+++ src/js/modules/Events.js
@@ -1,63 +1,60 @@
-import { getEventTarget } from './Helpers';
-import initHandler from './initHandler';
-import PathResolver from './PathResolver';
+import { getEventTarget } from "./Helpers";
+import initHandler from "./initHandler";
+import PathResolver from "./PathResolver";
 
 const Events = {};
 
 let handlers = {};
 
+initHandler.onInit(function () {
+  document.addEventListener("input", (e) => Events.trigger(e.type, e));
+  document.addEventListener("keyup", (e) => Events.trigger(e.type, e), true);
+  document.addEventListener("contextmenu", (e) => Events.trigger(e.type, e));
+  document.addEventListener("mousedown", (e) => Events.trigger(e.type, e));
+  document.addEventListener("dblclick", (e) => Events.trigger(e.type, e));
+  document.addEventListener("click", (e) => Events.trigger(e.type, e));
+  document.addEventListener("focus", (e) => Events.trigger(e.type, e), true);
+  document.addEventListener("blur", (e) => Events.trigger(e.type, e), true);
+  document.addEventListener("change", (e) => Events.trigger(e.type, e), true);
+  document.addEventListener("keydown", (e) => Events.trigger(e.type, e), true);
 
-initHandler.onInit(function() {
-	document.addEventListener('input', (e) => 		Events.trigger(e.type, e) );
-	document.addEventListener('keyup', (e) => 		Events.trigger(e.type, e), true );
-	document.addEventListener('contextmenu', (e) =>Events.trigger(e.type, e) );
-	document.addEventListener('mousedown', (e) => 	Events.trigger(e.type, e) );
-	document.addEventListener('dblclick', (e) => 	Events.trigger(e.type, e) );
-	document.addEventListener('click', (e) => 		Events.trigger(e.type, e) );
-	document.addEventListener('focus', (e) =>		Events.trigger(e.type, e), true);
-	document.addEventListener('blur', (e) =>		Events.trigger(e.type, e), true);
-	document.addEventListener('change', (e) => 		Events.trigger(e.type, e), true);
-	document.addEventListener('keydown', (e) => 	Events.trigger(e.type, e), true);
+  window.addEventListener("message", function (msg) {
+    if (msg.data.action && msg.data.action === "terafmEventCatcher") {
+      msg = msg.data.terafmEvent;
+      const eventTarget = getEventTarget(msg);
+      if (!eventTarget) return;
 
-	window.addEventListener('message', function(msg) {
-		if(msg.data.action && msg.data.action === 'terafmEventCatcher') {
-			msg = msg.data.terafmEvent;
-			const eventTarget = getEventTarget(msg);
-			if (!eventTarget) return;
+      let target = PathResolver(eventTarget);
 
-			let target = PathResolver(eventTarget);
-
-			if(target) {
-				msg.path[0] = target;
-				Events.trigger(msg.type, msg);
-			}
-		}
-	})
+      if (target) {
+        msg.path[0] = target;
+        Events.trigger(msg.type, msg);
+      }
+    }
+  });
 });
 
+Events.trigger = function (type, event) {
+  if (type in handlers) {
+    for (let h = 0; h < handlers[type].length; ++h) {
+      handlers[type][h](event);
+    }
+  }
+};
 
-Events.trigger = function(type, event) {
-	if(type in handlers) {
-		for(let h =0; h < handlers[type].length; ++h) {
-			handlers[type][h](event);
-		}
-	}
-}
-
-
-Events.on = function(type, callback) {
-	if(Array.isArray(type)) {
-		for(let evt of type) {
-			attachListener(evt, callback);
-		}
-	} else {
-		attachListener(type, callback);
-	}
-}
+Events.on = function (type, callback) {
+  if (Array.isArray(type)) {
+    for (let evt of type) {
+      attachListener(evt, callback);
+    }
+  } else {
+    attachListener(type, callback);
+  }
+};
 
 function attachListener(type, callback) {
-	if(!Array.isArray(handlers[type])) handlers[type] = [];
-	handlers[type].push(callback);
+  if (!Array.isArray(handlers[type])) handlers[type] = [];
+  handlers[type].push(callback);
 }
 
 export default Events;
