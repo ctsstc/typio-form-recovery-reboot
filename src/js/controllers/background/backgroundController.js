e28a9370e427c79f3cbdab50a46de5436e8b3daf
Cody Swartz <ctsstc@gmail.com>
2024-03-28 10:35:51 -0700

    Fix extension reload errors
    This fixes the error for:
    ```
    Uncaught Error: Extension context invalidated.
    ```
    
    This helps with a development bug, when the code would try to access the storage after the extension host had been reloaded.
    
    See:
    - https://stackoverflow.com/questions/10994324/chrome-extension-content-script-re-injection-after-upgrade-or-install/11598753?answertab=trending#tab-top

--- src/js/controllers/background/backgroundController.js
+++ src/js/controllers/background/backgroundController.js
@@ -1,8 +1,32 @@
 // Controllers
+import "./contextMenuController.js";
 import "./listenerController.js";
 import "./maintenanceController.js";
-import "./contextMenuController.js";
 import "./splashController.js";
+
+chrome.runtime.onInstalled.addListener(async () => {
+  for (const cs of chrome.runtime.getManifest().content_scripts) {
+    for (const tab of await chrome.tabs.query({ url: cs.matches })) {
+      if (tab.url.match(/(chrome|chrome-extension):\/\//gi)) {
+        continue;
+      }
+
+      chrome.scripting
+        .executeScript({
+          files: cs.js,
+          target: { tabId: tab.id, allFrames: cs.all_frames },
+          injectImmediately: cs.run_at === "document_start",
+          world: cs.world, // uncomment if you use it in manifest.json in Chrome 111+
+        })
+        .catch((e) => {
+          console.warn("Unable to reload Form Recovery Script", {
+            e,
+            url: tab.url,
+          });
+        });
+    }
+  }
+});
 
 // In case i need it for future stuff
 chrome.storage.sync.set({ version: 2 });
